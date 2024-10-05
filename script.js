5a6f53f233ec1628e52c98a9efb75a7f49055f3e
Mohamad nwilaty <mhmadnwilaty@gmail.com>
2024-10-05 21:18:51 +0300

    made it pretty and fun

--- script.js
+++ script.js
@@ -5,21 +5,23 @@ const height = innerHeight
 canvas.width = width;
 canvas.height = height ;
 let gameState = true ;
-const enemyColors = ["black", "lightgrey" , "red" , "yellow" , "purple" , "green" , "pink"];
+let score = 0 ;
+let bestScore = localStorage.getItem("bestScore") || 0 ;
+const enemyColors = ["blue", "grey" , "red" , "yellow" , "purple" , "green" , "deeppink" , "aqua" , "chartreuse"];
 let bullets = [];
 let enemies = [];
-
+let particles = [];
 
 
 class Player{
     constructor(){
         this.x = width/2 ;
         this.y = height / 2 ;
-        this.radius = 30 ;
+        this.radius = 20 ;
     }
     Draw(){
         ctx.beginPath();
-        ctx.fillStyle = "blue" ;
+        ctx.fillStyle = "ghostwhite" ;
         ctx.arc(this.x , this.y , this.radius , 0 , Math.PI *2 , false);
         ctx.fill()
         ctx.closePath();
@@ -32,10 +34,11 @@ class Enemy{
         this.radius = radius ;
         this.angle = angle
         this.color = enemyColors[Math.floor(Math.random()*enemyColors.length)]
+        this.speed =  1 / radius * 100; // Adjust the multiplier as needed
     }
     update(){
-        this.x += 2 * Math.cos(this.angle);
-        this.y += 2 * Math.sin(this.angle);
+        this.x += this.speed * Math.cos(this.angle);
+        this.y += this.speed * Math.sin(this.angle);
     }
     Draw(){
         ctx.beginPath();
@@ -47,9 +50,9 @@ class Enemy{
     }
 }
 class Bullet{
-    constructor(w , h ,angle){
-        this.x = w ;
-        this.y = h ;
+    constructor(x , y ,angle){
+        this.x = x ;
+        this.y = y ;
         this.radius = 5 ;
         this.angle = angle ;
     }
@@ -59,20 +62,55 @@ class Bullet{
     }
     Draw(){
         ctx.beginPath();
-        ctx.fillStyle = "red" ;
+        ctx.fillStyle = "white" ;
         ctx.arc(this.x , this.y , this.radius , 0 , Math.PI *2 , false);
         ctx.fill();
         ctx.closePath();
         this.update()
     }
-    
 }
+
+class particle{
+    constructor(x , y ,velocity , color){
+        this.x = x ;
+        this.y = y ;
+        this.radius = Math.random() *2 ;
+        this.velocity = velocity ;
+        this.color = color;
+        this.alpha = 1 ;
+    }
+    update(){
+        this.x += 2 * this.velocity.x ;
+        this.y += 2 * this.velocity.y ;
+        this.alpha -= 0.01 ;
+    }
+    Draw(){
+        ctx.save();
+        ctx.beginPath();
+        ctx.globalAlpha = this.alpha
+        ctx.fillStyle = this.color ;
+        ctx.arc(this.x , this.y , this.radius , 0 , Math.PI *2 , false);
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+        this.update()
+    }
+}
+
 
 const p = new Player();
 spawnEnimies();
 requestAnimationFrame(animate) ;
 
 window.addEventListener("click" , (e)=>{
+    if(!gameState){
+        gameState = true ;
+        bullets = [];
+        enemies = [];
+        particles = [] ;
+        score = 0 ;
+        requestAnimationFrame(animate)
+    }
     const b = new Bullet(p.x, p.y , Math.atan2(p.y - e.offsetY ,p.x - e.offsetX));
     bullets.push(b);
 })
@@ -81,7 +119,7 @@ function clearCanvas(){
     ctx.clearRect(0,0,width,height);
 }
 function animate(){
-    clearCanvas();
+    drawBackground()
     for(let i=0 ; i<bullets.length ; i++){
         bullets[i].Draw();
         if(bulletCollisionWEnemy(bullets[i])){
@@ -96,21 +134,34 @@ function animate(){
     for(let i = 0 ; i < enemies.length ; i++){
         enemies[i].Draw();
     }
-    p.Draw();
 
+    particles.forEach(( p , index) =>{
+        if(p.alpha < 0){
+            particles.splice(index , 1)
+        }
+        else{
+            p.Draw() ;
+        }
+        
+    })
+
+    p.Draw();
+    drawScore()
     checkGameOver();
 
-    
-    console.log(enemies.length , bullets.length);
-    if(gameState)[
+    console.log(particles.length)
+
+    if(gameState){
         requestAnimationFrame(animate) 
-    ]
+    }
 }
 function checkOutofBounds(b){
     if(b.x + b.radius < 0 || b.x + b.radius > width){
+        score -= 200 ;
         return true ;
     }
     if(b.y + b.radius< 0 || b.y + b.radius > height){
+        score -= 200 ;
         return true ;
     }
     return false ;
@@ -140,7 +191,14 @@ function bulletCollisionWEnemy(b){
     for(let i=0 ; i<enemies.length ; i++){
         if(b.x < enemies[i].x + enemies[i].radius && b.x > enemies[i].x - enemies[i].radius  ){
             if(b.y < enemies[i].y + enemies[i].radius && b.y > enemies[i].y - enemies[i].radius){
+                // spawn particles for dying enemies
+                for(let j=0 ; j<enemies[i].radius *2 ; j++){
+                    particles.push(new particle(enemies[i].x , enemies[i].y , {x:Math.random() - 0.5 , y:Math.random() - 0.5} ,enemies[i].color))
+                }
                 enemies.splice(i,1);
+                score += 100;
+                
+                console.log(enemies[i].x)
                 return true ;
             }
         }
@@ -159,7 +217,31 @@ function checkGameOver() {
             // Collision detected, game over
             gameState = false;
             console.log("Game Over");
+            deathscreen()
             break;
         }
     }
+}
+function drawBackground(){
+    ctx.beginPath();
+    ctx.fillStyle = "rgba(0,0,0,0.15)" ;
+    ctx.fillRect(0,0,width,height);
+    ctx.closePath();
+}
+function drawScore(){
+    ctx.font = "36px pixel-font";
+    ctx.fillStyle = "white" ;
+    ctx.fillText(score , 50 ,50 )
+}
+function deathscreen(){
+    if(score > bestScore){
+        bestScore = score;
+        localStorage.setItem("bestScore" , bestScore);
+    }
+    ctx.font = "60px pixel-font";
+    ctx.textAlign = "center";
+    ctx.fillStyle = "white";
+    ctx.fillText(`score: ${score}` , width/2 , height/2 -70);
+    ctx.fillText(`best score: ${bestScore}` , width/2 , height/2 );
+    ctx.fillText("click to restart" , width/2 , height/2 +70 );
 }
